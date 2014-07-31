be46a559358850dbeb5a484de8444d3fcae1eb7c
xhuang3 <xhuang3@binghamton.edu>
2014-07-30 21:23:28 -0400

    added refresh

--- Data.cs
+++ Data.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WhatsOnHerMind
 {
@@ -11,6 +7,8 @@ namespace WhatsOnHerMind
     {
         public string DateString { get; set; }
         public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
 
         public double Data { get; set; }
 
@@ -32,49 +30,47 @@ namespace WhatsOnHerMind
             if (date1.Year == date2.Year && date1.Month == date2.Month && date1.Day == date2.Day) return true;
             return false;
         }
-        public static void PopulateList(DateTime date)
+
+
+        public void PopulateList(DateTime date)
         {
-            if (_DataObjectList == null)
-            {
-                _DataObjectList = new DataObjectList();
-            }
             _DataObjectList.Clear();
-            DateTime angryDate = new DateTime();
+
+            DateTime aDate = new DateTime();
             DateTime pDate = new DateTime();
             if (DateTimeList.GetDateTimeList().Count > 2)
             {
                 pDate = DateTimeList.GetDateTimeList()[DateTimeList.GetDateTimeList().Count - 1];
-                angryDate = pDate.AddDays(14);
+                aDate = pDate.AddDays(14);
                 pDate = pDate.AddDays(DateTimeList.AvgDiff());
             }
-            int angryI = 0;
-            int pI = 0;
+            int angryI = -1;
+            int pI = -1;
+
             for (int i = 0; i < 30; ++i)
             {
                 DateTime tempDate = date.AddDays(i);
-                DataObject dataObject = new DataObject(tempDate, 1);
-                if(CompareDateTime(tempDate, angryDate))
+                if(CompareDateTime(tempDate, aDate))
                 {
                     angryI = i;
                 }
-                if(CompareDateTime( tempDate, pDate))
+                if(CompareDateTime(tempDate, pDate))
                 {
                     pI = i;
                 }
+                DataObject dataObject = new DataObject(tempDate, 1);
                 _DataObjectList.Add(dataObject);
             }
-
-            int offset = 0;
-            for(int i = angryI; i < pI; ++i)
+            
+            if (angryI < pI && angryI > 0)
             {
-                _DataObjectList[i].Data = 3 + offset * 0.2;
-                offset++;
-            }
-            offset = 0;
-            for(int i = 0; i < 6; ++i)
-            {
-                _DataObjectList[pI + i].Data = 3 + (pI - angryI) * 0.2 - offset * 0.2;
-                offset++;
+                for(int i = 0; i < 30; ++i)
+                {
+                    if (i < angryI) _DataObjectList[i].Data += i * 0.05;
+                    else if (angryI <= i && i < pI) _DataObjectList[i].Data = 3 + i * 0.1;
+                    else if (i >= pI && i < pI + 7) _DataObjectList[i].Data -= i * 0.01;
+                    else _DataObjectList[i].Data = 1;
+                }
             }
         }
 
--- MainAppPage.xaml.cs
+++ MainAppPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Phone.Controls;
+using Microsoft.Phone.Scheduler;
 using System;
 using System.Collections.ObjectModel;
 
@@ -6,8 +7,6 @@ namespace WhatsOnHerMind
 {
     public partial class MainAppPage : PhoneApplicationPage
     {
-        private ObservableCollection<DataObject> _dataList = new ObservableCollection<DataObject>();
-
         public ObservableCollection<DataObject> DataList { get; set; }
 
         public MainAppPage()
@@ -25,17 +24,36 @@ namespace WhatsOnHerMind
             DateTimeList.GetDateTimeList().AddDateTime(date);
 
             int avgDay = DateTimeList.AvgDiff();
-            if(avgDay > 0 && DateTimeList.GetDateTimeList().Count > 2){
+            if(DateTimeList.GetDateTimeList().Count > 2){
                 // TODO: Debug here!!!!
                DateBlock.Text = (DateTimeList.GetDateTimeList()[DateTimeList.GetDateTimeList().Count - 1].AddDays(avgDay)).ToString();
-               DataObjectList.PopulateList(DateTime.Now);
-               NavigationService.Navigate(new Uri("/MainAppPage.xaml?Refresh=true", UriKind.Relative));
+               DataObjectList.GetDataObjectList().PopulateList(DateTime.Now);
             }
             else
             {
                 DateBlock.Text = "Not enough information!";
             }
             
+        }
+
+        private void RefreshButtonPressed(object sender, System.Windows.RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainAppPage.xaml?Refresh=true", UriKind.Relative));
+        }
+
+        private void AddReminderButtonPressed(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var reminder = new Reminder("my reminder")
+            {
+                Title = "my title",
+                Content = "my content",
+                BeginTime = DateTime.Now.AddMinutes(1),
+                ExpirationTime = DateTime.Now.AddHours(1),
+                RecurrenceType = RecurrenceInterval.None,
+                NavigationUri = null
+            };
+
+            ScheduledActionService.Add(reminder);
         }
     }
 }
